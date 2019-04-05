b3407b66a14e6495d09f30adf6fc1f11bf498988
Bruno Pinheiro <brunopinheiro.net@gmail.com>
2019-04-05 10:25:19 -0300

    data remake

--- data-raw/06_od_viagens.R
+++ data-raw/06_od_viagens.R
@@ -3,9 +3,8 @@ library(dplyr)
 library(sf)
 library(asha)
 
-load("data-raw/setores_censitarios-raw.rda")
+data("setores")
 data("ubs_pontos")
-
 
 # PONTOS DE ORIGEM (CENTROIDES DOS SETORES) -----------------------------------
 centroides <- st_centroid(setores) %>% select(cd_geocodi)
@@ -15,6 +14,54 @@ centroides <- st_centroid(setores) %>% select(cd_geocodi)
 # No caso estamos trabalhando com as 5 UBS mais proximas.
 od_euclidiano <- asha_nn(ubs_pontos, centroides, "cnes", "cd_geocodi", 5) %>%
   as_tibble()
+
+# odprox_euc <- list(
+#   setor = od_euclidiano %>% filter(proximidade == 1) %>%
+#     arrange(cd_geocodi) %>% select(cd_geocodi) %>%
+#     left_join(as_tibble(centroides), by = "cd_geocodi") %>%
+#     st_sf() %>% st_transform(4326),
+#   ubs = od_euclidiano %>% filter(proximidade == 1) %>%
+#     arrange(cd_geocodi) %>% select(cnes) %>%
+#     left_join(as_tibble(ubs_pontos), by = "cnes") %>%
+#     st_sf() %>% st_transform(4326)
+#   )
+
+# setor <- as.matrix(st_coordinates(st_transform(odprox_na$setor[1:3, ], 4326)))
+# ubs <- as.matrix(st_coordinates(st_transform(odprox_na$ubs[1:3, ], 4326)))
+
+asha_dist <- function(from, to, mode, api) {
+  if ("sf" %in% class(from)) {
+    from = as.matrix(st_coordinates(st_transform(from, 4326)))
+    to = as.matrix(st_coordinates(st_transform(to, 4326)))
+  }
+
+  dist <- lapply(1:nrow(from), function(i) {
+    from = from[i, ]
+    to = to[i, ]
+    stplanr::dist_google(from = from, to = to, mode = mode, google_api = api)
+    print(i)
+  })
+  dist <- do.call(rbind, dist)
+  return(dist)
+}
+
+odvig_na <- list(
+  setor = od_indicadores %>% filter(is.na(distancia), malha == "vigente") %>%
+    arrange(cd_geocodi) %>% select(cd_geocodi, cnes) %>%
+    left_join(as_tibble(centroides), by = "cd_geocodi") %>%
+    st_sf() %>% st_transform(4326),
+  ubs = od_indicadores %>%  filter(is.na(distancia), malha == "vigente") %>%
+    arrange(cd_geocodi) %>% select(cnes) %>%
+    left_join(as_tibble(ubs_pontos), by = "cnes") %>%
+    st_sf() %>% st_transform(4326)
+  )
+
+zonasvig <- asha_zones(odvig_na$setor, distinct(odvig_na$ubs, cnes), "cd_geocodi", "cnes")
+od_viagens <- asha_dists(odvig_na$setor, zonasvig, modal = "walking")
+
+od_viagens_na <- as_tibble(odvig_na$setor) %>%
+  select(-geometry) %>%
+  cbind(od_viagens)
 
 # LEVANTAR ROTAS --------------------------------------------------------------
 # O levantamento de dados de rotas consiste na consulta à Google Distance
@@ -26,7 +73,11 @@ zonas <- asha_zones(centroides, ubs_pontos, "cd_geocodi", "cnes")
 
 ## Levantar dados de rotas ---------------
 # Esta etapa demora muito. Mais de um dia.
-od_viagens <- asha_dists(od_euclidiano, zonas)
+od_viagens <- asha_dists(od_euclidiano, zonas, modal = "walking")
+
+
+
+
 
 ## Renomear as varíávies
 od_viagens <- od_viagens %>%

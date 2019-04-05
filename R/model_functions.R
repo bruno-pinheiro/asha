4c90c592d0483904b47c38e4318955f5884fc594
Bruno Pinheiro <brunopinheiro.net@gmail.com>
2019-04-05 10:23:53 -0300

    asha_ind_group() function included

--- R/model_functions.R
+++ R/model_functions.R
@@ -39,6 +39,7 @@ asha_intersect <- function(sf1, sf2, id1, id2) {
     dplyr::select(id2, id1)
 }
 
+
 #' @title Encontrar os n pontos mais proximos
 #' @name asha_nn
 #'
@@ -92,156 +93,6 @@ asha_nn <- function(sf1, sf2, id1, id2, n) {
   return(df)
 }
 
-#' @title Obter informacoes de distancia e tempo de viagem por modal
-#' @name asha_dists
-#'
-#' @description Permite o levantamento de dados de rotas de viagem para
-#'              modais de transporte entre dois conjuntos de pontos espaciais
-#'
-#' @param fluxo Uma matriz ou data frame de duas colunas representando a
-#'              latitude e a longitude das origens.
-#' @param zonas Uma matriz ou data frame de duas colunas representando a
-#'              latitude e a longitude dos destinos.
-#' @param modal String especificando o modo de transporte, que pode ser bicycling (padrao),
-#'              walking, driving ou transit
-#' @param api String com API do Google Distance Matrix API
-#'
-#' @details Converte o CRS do objeto passado em \code{zonas} em lat long (4326) para
-#'          consultar a Google Matrix Distance API com funcao \code{\link[stplanr]{dist_google}}.
-#'          A consulta pode ser feita para os modais caminhada, transporte publico,
-#'          bicicleta e carro. Prepara automaticamente os objeto OD com
-#'          \code{\link[stplanr]{od2odf}} e gera resultados para pares linha a linha,
-#'          ao inves de todos para todos como o pacote stplanr, reduzindo o consumo da API.
-#'
-#' @return Retorna um data frame com colunas \code{de} (enderecos de origem),
-#'         \code{para} (enderecos de destino), \code{distancias} (em metros),
-#'         \code{tempo} (em segundos), \code{moeda} e \code{tarifa}
-#'
-#' @author Bruno Pinheiro
-#'
-#' @seealso \code{\link[stplanr]{dist_google}},
-#'          \code{\link[stplanr]{od2odf}}
-#'
-#' @examples
-#' \dontrun{
-#' cent <- sf::st_centroid(setores)
-#' x <- asha_nn(ubs_pontos, setores, "cnes", "cd_geocodi", 1)
-#' zonas <- asha_zones(cent, ubs_pontos, "cd_geocodi", "cnes")
-#' asha_dists(x[500:501, ], zonas, "transit")
-#' }
-#'
-#' @export
-asha_dists <- function(fluxo, zonas, modal = "walking", api) {
-  from_addresses = NULL
-  to_addresses = NULL
-  distances = NULL
-  duration = NULL
-  currency = NULL
-  fare = NULL
-  zonas <- methods::as(sf::st_transform(zonas, 4326), "Spatial")
-  od <- stplanr::od2odf(flow = fluxo, zones = zonas)
-  uma_linha <- data.frame(from_addresses = NA, to_addresses = NA,
-                          distances = NA, duration = NA,
-                          currency = NA, fare = NA)
-  output <- data.frame()
-  if(missing(api)) {
-    for (linha in 1:nrow(od)) {
-      o <- od[linha, 3:4]
-      d  <- od[linha, 5:6]
-      output <- tryCatch(
-        {
-          rbind(output, stplanr::dist_google(from = o, to = d,
-                                             mode = modal))
-        },
-        error = function(na) {
-          message(na)
-          output <- rbind(output, uma_linha)
-        }
-      )
-    }
-    return(output <- output %>%
-             dplyr::rename(de = from_addresses, para = to_addresses,
-                           distancias = distances, tempo = duration,
-                           moeda = currency, tarifa = fare))
-    } else if(modal == "walking" & missing(api)) {
-      for (linha in 1:nrow(od)) {
-        o <- od[linha, 3:4]
-        d  <- od[linha, 5:6]
-        output <- tryCatch(
-          {
-            rbind(output, stplanr::dist_google(from = o, to = d,
-                                               mode = modal))
-          },
-          error = function(na) {
-            message(na)
-            output <- rbind(output, uma_linha)
-          }
-        )
-      }
-      return(output <-output %>%
-               dplyr::rename(de = from_addresses, para = to_addresses,
-                             distancias = distances, tempo = duration,
-                             moeda = currency, tarifa = fare))
-      } else if(modal == "walking") {
-        for (linha in 1:nrow(od)) {
-          o <- od[linha, 3:4]
-          d  <- od[linha, 5:6]
-          output <- tryCatch(
-            {
-              rbind(output, stplanr::dist_google(from = o, to = d,
-                                                 mode = modal, google_api = api))
-            },
-            error = function(na) {
-              message(na)
-              output <- rbind(output, uma_linha)
-            }
-          )
-        }
-        return(output <- output %>%
-                 dplyr::rename(de = from_addresses, para = to_addresses,
-                               distancias = distances, tempo = duration,
-                               moeda = currency, tarifa = fare))
-      } else if(missing(modal) & missing(api)) {
-            for (linha in 1:nrow(od)) {
-              o <- od[linha, 3:4]
-              d  <- od[linha, 5:6]
-              output <- tryCatch(
-                {
-                  rbind(output, stplanr::dist_google(from = o, to = d))
-                },
-                error = function(na) {
-                  message(na)
-                  output <- rbind(output, uma_linha)
-                }
-              )
-            }
-            return(output <-output %>%
-                     dplyr::rename(de = from_addresses, para = to_addresses,
-                                   distancias = distances, tempo = duration,
-                                   moeda = currency, tarifa = fare))
-        } else {
-    for (linha in 1:nrow(od)) {
-      o <- od[linha, 3:4]
-      d  <- od[linha, 5:6]
-      output <- tryCatch(
-        {
-          rbind(output, stplanr::dist_google(from = o, to = d,
-                                           mode = modal, google_api = api))
-          },
-        error = function(na) {
-          message(na)
-          output <- rbind(output, uma_linha)
-          }
-      )
-    }
-    }
-  return(output <-
-           output %>%
-           dplyr::rename(de = from_addresses, para = to_addresses,
-                         distancias = distances, tempo = duration,
-                         moeda = currency, tarifa = fare))
-}
-
 
 #' @title Estrutura a base de zonas necessaria para usar \code{asha_dists}
 #' @name asha_zones
@@ -285,7 +136,6 @@ asha_zones <- function(sf1, sf2, id1, id2) {
   )
 }
 
-# Funcao asha_ac ###########################
 
 #' @title Indicador AC
 #' @name asha_ac
@@ -335,6 +185,7 @@ asha_ac <- function(df1, pop, area, model, n = 1) {
       dplyr::mutate(ac = (oportunidades / demanda) * n)
   }
 }
+
 
 #' @title Indicador AV
 #' @name asha_av
@@ -411,3 +262,53 @@ asha_av <- function(df, id, segundos, pop, model, raio) {
   }
 }
 
+
+#' @title Calcular indicadores agregados
+#' @name asha_ind_group
+#'
+#' @description Calcula população agregada por categorias
+#'
+#' @param df Um dataframe contendo as variaveis necessarias para o calculo. Veja os detalhes.
+#' @param ind Variavel categórica com o indicador
+#' @param pop Variavel populacional
+#' @param group Variável para agregar, além do indicador (opcional)
+#' @param na.rm Útil caso a variável passada em \code{ind} contenha NA (default is FALSE)
+#'
+#' @details Descrever
+#'
+#' @return Descrever
+#'
+#' @author Bruno Pinheiro
+#'
+#' @examples
+#' \dontrun{
+#' asha_ind_group(od_indicadores, ac_cat, habitantes, malha, na.rm = TRUE)
+#' }
+#'
+#' @export
+asha_ind_group <- function(df, ind, pop, group, na.rm = FALSE){
+  ind <- rlang::enquo(ind)
+  pop <- rlang::enquo(pop)
+
+  if (na.rm == TRUE){
+    df <- df %>% dplyr::filter(!is.na(!! ind))
+  }
+
+  if (missing(group)) {
+
+    df <- df %>%
+      dplyr::group_by(!! ind) %>%
+      dplyr::summarise(pop = sum(!! pop, na.rm = TRUE)) %>%
+      dplyr::mutate(prop = prop.table(pop)) %>%
+      dplyr::ungroup()
+
+  } else {
+    group <- rlang::enquo(group)
+    df <- df %>%
+      dplyr::group_by(!! group, !! ind) %>%
+      dplyr::summarise(pop = sum(!! pop, na.rm = TRUE)) %>%
+      dplyr::mutate(prop = prop.table(pop)) %>%
+      dplyr::ungroup()
+  }
+  return(df)
+}
